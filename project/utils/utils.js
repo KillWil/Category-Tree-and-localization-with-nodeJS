8af5e7c17cfa5a5b02d4e216fe194dab7479f7db
Billiouw <wilfrid.billiouw@aphelie.com>
2012-01-30 14:57:52 +0100

    Little bug in Response format

--- project/utils/utils.js
+++ project/utils/utils.js
@@ -4,15 +4,15 @@
 
   exports.ErrorManagement = function(req, res, err, doc) {
     if (err) {
-      switch (err.type) {
+      switch (err.Type) {
         case "ENOTFOUND":
-          return res.respond(err.message, 404);
+          return res.respond(err.Message, 404);
         case "ENOTIMPLEMENTED":
-          return res.respond(err.message, 501);
+          return res.respond(err.Message, 501);
         case "EVALIDATION":
-          return res.respond(err.message, 409);
+          return res.respond(err.Message, 409);
         default:
-          return res.respond(err.message, 500);
+          return res.respond(err.Message, 500);
       }
     } else {
       return res.respond(doc, 200);
